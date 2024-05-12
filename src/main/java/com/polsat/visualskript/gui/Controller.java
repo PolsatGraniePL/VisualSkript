913835e9c6535752efedd62a2f20bc4b832b1451
Polsat GraniePL <k.ksiazek.yt@gmail.com>
2024-05-12 12:33:02 +0200

    Wstawiono putBlocksInContainer() do BlockManager.java oraz zmieniono Button na Pane jako forma bloków

--- src/main/java/com/polsat/visualskript/gui/Controller.java
+++ src/main/java/com/polsat/visualskript/gui/Controller.java
@@ -59,7 +59,8 @@ public class Controller {
 
         TabManager.loadLatestTab(buildTab);
         loadDefaultBlockList();
-        putBlocksInContainer(blocksList);
+        BlockManager.setupTabPane(buildTab, blockContainer);
+        BlockManager.putBlocksInContainer(blocksList);
         sortBlocksList();
     }
 
@@ -77,50 +78,10 @@ public class Controller {
     public void sortBlocksList() {
         blockContainer.getChildren().clear();
         List<Block> visibleBlocksList = BlockManager.sortBlocksList(blocksList, textField.getText().toLowerCase(), checkBoxes);
-        putBlocksInContainer(visibleBlocksList);
+        BlockManager.putBlocksInContainer(visibleBlocksList);
     }
 
-    private void putBlocksInContainer(List<Block> listType){
-        for (int i = 0; i < listType.size(); i++) {
-            Button tmpBtn = new Button(listType.get(i).getName());
-            tmpBtn.setStyle("-fx-border-color: #000000; -fx-border-radius: 2px ; -fx-background-color: #"+ listType.get(i).getType().getHexColor() +"; -fx-font-color: #000000; -fx-font-weight: bold; -fx-font-size: 15px; ");
-            tmpBtn.setTooltip(new Tooltip(listType.get(i).getDescription()+"\n\n"+listType.get(i).getPattern()));
 
-            tmpBtn.setOnDragDetected(event -> {
-                Dragboard dragboard = tmpBtn.startDragAndDrop(TransferMode.ANY);
-
-                ClipboardContent content = new ClipboardContent();
-                content.putString(tmpBtn.getText());
-                dragboard.setContent(content);
-
-                dragboard.setDragView(tmpBtn.snapshot(null, null));
-
-                event.consume();
-            });
-
-            buildTab.setOnDragOver(event -> {
-                if (event.getGestureSource() != buildTab && event.getDragboard().hasString()) {
-                    event.acceptTransferModes(TransferMode.ANY);
-                }
-                event.consume();
-            });
-
-            buildTab.setOnDragDropped(event -> {
-                Dragboard db = event.getDragboard();
-                boolean success = false;
-                if (db.hasString()) {
-                    System.out.println(db.getString());
-                    System.out.println(buildTab.getSelectionModel().getSelectedItem().getText());
-                    success = true;
-                }
-                event.setDropCompleted(success);
-                event.consume();
-            });
-
-            VBox.setMargin(tmpBtn, new Insets(10, 10, 10, 10));
-            blockContainer.getChildren().add(tmpBtn);
-        }
-    }
 
 //  Menu Controller
     public void menuCreateScript(){
--- src/main/java/com/polsat/visualskript/gui/manager/block/BlockManager.java
+++ src/main/java/com/polsat/visualskript/gui/manager/block/BlockManager.java
@@ -2,7 +2,15 @@ package com.polsat.visualskript.gui.manager.block;
 
 import com.polsat.visualskript.gui.block.Block;
 import com.polsat.visualskript.gui.block.BlockType;
-import javafx.scene.control.CheckBox;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -12,6 +20,10 @@ import java.util.*;
 
 public class BlockManager {
 
+    private static TabPane buildTab;
+    private static VBox blockContainer;
+
+    //Zwraca listę bloków z pliku SkriptDocs.json
     public static List<Block> getBlocksList(){
         List<Block> list = new ArrayList<>();
         try {
@@ -80,6 +92,7 @@ public class BlockManager {
         return list;
     }
 
+    //Zwraca listę bloków, zgodnie z tekstem lub typem bloku.
     public static List<Block> sortBlocksList(List<Block> blocksList, String text, List<CheckBox> checkBoxes) {
         List<Block> visibleBlocksList = new ArrayList<>();
         blocksList.sort(Comparator.comparing(Block::getType));
@@ -95,5 +108,67 @@ public class BlockManager {
         return visibleBlocksList;
     }
 
+    public static void setupTabPane(TabPane buildTab, VBox blockContainer){
+        BlockManager.buildTab = buildTab;
+        BlockManager.blockContainer = blockContainer;
+    }
+
+    //Wstawia listę bloków do VBox
+    public static void putBlocksInContainer(List<Block> listType){
+        for (int i = 0; i < listType.size(); i++) {
+
+            //Create objects
+            Pane tmpPane = new Pane();
+            Label tmpLabel = new Label(listType.get(i).getName());
+
+            //Style objects
+            tmpPane.getChildren().add(tmpLabel);
+            tmpPane.setStyle("-fx-border-color:  #020202; -fx-border-radius: 5px ; -fx-background-color: #"+ listType.get(i).getType().getHexColor() +"; -fx-background-radius: 5px; -fx-border-width: 1px");
+            tmpLabel.setStyle("-fx-font-color: #000000; -fx-font-weight: bold; -fx-font-size: 14px;");
+            tmpLabel.setTooltip(new Tooltip(listType.get(i).getDescription()+"\n\n"+listType.get(i).getPattern()));
+
+            VBox.setMargin(tmpPane, new Insets(10, 10, 10, 10));
+            tmpLabel.setPadding(new Insets(5, 5, 5, 5));
+
+            tmpPane.setPrefWidth(Region.USE_COMPUTED_SIZE);
+            tmpPane.setMinWidth(Region.USE_PREF_SIZE);
+            tmpPane.setMaxWidth(Region.USE_PREF_SIZE);
+
+            //Object system
+            tmpPane.setOnDragDetected(event -> {
+                Dragboard dragboard = tmpPane.startDragAndDrop(TransferMode.ANY);
+
+                ClipboardContent content = new ClipboardContent();
+                content.putString(tmpLabel.getText());
+                dragboard.setContent(content);
+
+                dragboard.setDragView(tmpPane.snapshot(null, null));
+
+                event.consume();
+            });
+
+            buildTab.setOnDragOver(event -> {
+                if (event.getGestureSource() != buildTab && event.getDragboard().hasString()) {
+                    event.acceptTransferModes(TransferMode.ANY);
+                }
+                event.consume();
+            });
+
+            buildTab.setOnDragDropped(event -> {
+                Dragboard db = event.getDragboard();
+                boolean success = false;
+                if (db.hasString()) {
+                    System.out.println(db.getString());
+                    System.out.println(buildTab.getSelectionModel().getSelectedItem().getText());
+                    success = true;
+                }
+                event.setDropCompleted(success);
+                event.consume();
+            });
+
+            //Add object to block container
+            blockContainer.getChildren().add(tmpPane);
+        }
+    }
 
 }
