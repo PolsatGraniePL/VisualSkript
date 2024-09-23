064c55afb368760e97568a0e123dfd6c0a5a4d21
PolsatGraniePL <k.ksiazek.yt@gmail.com>
2024-09-23 19:35:57 +0200

    open file status.

--- src/main/java/com/polsat/visualskript/system/script/ScriptManager.java
+++ src/main/java/com/polsat/visualskript/system/script/ScriptManager.java
@@ -3,13 +3,32 @@ package com.polsat.visualskript.system.script;
 import com.polsat.visualskript.util.ErrorHandler;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ScriptManager {
 
     public static void setOpened(File file, boolean status){
-        System.out.println("Opened na " + status + " dla " + file.getName());
         try {
-            //SKRIPT OPTIONS: status
+            Scanner scanner = new Scanner(file);
+            ArrayList<String> list = new ArrayList<>();
+
+            while (scanner.hasNextLine()) {
+                list.add(scanner.nextLine());
+            }
+            scanner.close();
+
+            if (!list.isEmpty() && list.get(0).startsWith("#status: ")) {
+                list.remove(0);
+            }
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write("#status: "+status + "\n");
+            for (String line : list) {
+                fileWriter.write(line + "\n");
+            }
+            fileWriter.close();
         } catch (Exception e){
             ErrorHandler.alert(e.toString());
         }
@@ -17,12 +36,18 @@ public class ScriptManager {
 
     public static boolean getOpened(File file){
         try {
-            //GET SKRIPT OPTIONS: status
+            Scanner scanner = new Scanner(file);
+            ArrayList<String> list = new ArrayList<>();
+
+            while (scanner.hasNextLine()) {
+                list.add(scanner.nextLine());
+            }
+            scanner.close();
+
+            return Boolean.parseBoolean(list.get(0).replace("#status: ",""));
         } catch (Exception e){
-            ErrorHandler.alert(e.toString());
             return false;
         }
-        return true;
     }
 
 }
